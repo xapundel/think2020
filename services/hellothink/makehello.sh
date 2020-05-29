cd39c2ab3b1250c59b657b676f3f2df07aa1c339
Dmitry Gorbachev <dmitry.gorbachev90@gmail.com>
2020-05-30 01:25:24 +0300

    services & config files published

---
+++ services/hellothink/makehello.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Service for MQTT message publication using Mosquitto client
+echo "[hellothink] Service was started"
+
+TOPIC_NAME=thinkmoscow2020/${HZN_DEVICE_ID}/hello
+
+echo "${USER_EMAIL}" > msg
+mosquitto_pub --insecure -L ${MQTT_BROKER_URI}/${TOPIC_NAME} -q 1 -f msg
+if [ $? -eq 0 ]; then
+  echo "[hellothink] hello msg published to MQTT"
+else
+  echo "[hellothink] MQTT publication error"
+fi
+
+sleep 86400 # do not finish process immediately
